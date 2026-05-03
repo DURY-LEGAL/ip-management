dcd555d52229f493d67e05819939ad15b47694e5
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:29 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// ip-management - Intellectual property management toolkit - Patents, trademarks, designs. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
